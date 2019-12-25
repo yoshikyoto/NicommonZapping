416681088d1553df29d601c2f3ac4c630e5f8fa2
yoshiyuki-sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-12-25 10:28:18 +0900

    Initial Commit

---
+++ NicommonZapping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NicommonZapping
+//
+//  Created by 坂本　祥之　 on 2019/12/25.
+//  Copyright © 2019 yoshikyoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NicommonZappingTests/NicommonZappingTests.swift
@@ -0,0 +1,34 @@
+//
+//  NicommonZappingTests.swift
+//  NicommonZappingTests
+//
+//  Created by 坂本　祥之　 on 2019/12/25.
+//  Copyright © 2019 yoshikyoto. All rights reserved.
+//
+
+import XCTest
+@testable import NicommonZapping
+
+class NicommonZappingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
