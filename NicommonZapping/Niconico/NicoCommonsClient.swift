e08ef9c9e08dcb59ba61683c9302f76bb9cadfeb
yoshiyuki-sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-12-28 23:40:02 +0900

    コモンズAPIリクエスト

--- NicommonZapping/Niconico/NicoCommonsClient.swift
+++ NicommonZapping/Niconico/NicoCommonsClient.swift
@@ -2,24 +2,29 @@ import Foundation
 
 public class NicoCommonsClient {
     let urlSession: URLSession
-    let parser: NicoCommonsParser
+    let queryBuilder: QueryBuilder
+    let jsonDecoder: JSONDecoder
     
     public init(
         urlSession: URLSession = URLSession.shared,
-        parser: NicoCommonsParser = NicoCommonsParser()
+        queryBuilder: QueryBuilder = QueryBuilder(),
+        jsonDecoder: JSONDecoder = JSONDecoder()
     ) {
         self.urlSession = urlSession
-        self.parser = parser
+        self.queryBuilder = queryBuilder
+        self.jsonDecoder = jsonDecoder
+        self.jsonDecoder.dateDecodingStrategy = .iso8601
     }
     
     public func searchAudio() {
-        print("searchAudio")
         /// これはエラーにならないはずなので ! してしまう
-        var urlComponents = URLComponents(string: "https://commons.nicovideo.jp/search/material/audio")!
-        let queryItems = [
-            URLQueryItem(name: Sort.name, value: Sort.createdAt),
-            URLQueryItem(name: Order.name, value: Order.desc),
-        ]
+        var urlComponents = URLComponents(string: "https://public-api.commons.nicovideo.jp/v1/materials/search/tags")!
+        let queryItems = self.queryBuilder.build(
+            sort: Sort.START_TIME_DESC,
+            materialType: MaterialType.AUDIO,
+            permissionScopes: [PermissionScope.ALL],
+            commercialUses: [CommercialUse.OK]
+        )
         urlComponents.queryItems = queryItems
         
         /// エラーにならないはずなので ! してしまう
@@ -28,32 +33,106 @@ public class NicoCommonsClient {
         print(request)
         let task = self.urlSession.dataTask(with: request) { (data, urlResponse, error) in
             print("task result")
+            // TODO ステータスコード見たほうが良い気がする
             guard let data = data else {
+                // TODO エラー時の処理
                 print("Request Error")
                 return
             }
-            print("success")
-            print(data)
-            self.parser.parse(data: data)
+            // レスポンスのパース
+            do {
+                let response = try self.jsonDecoder.decode(NicoCommonsSearchResponse.self, from: data)
+                // TODO パースに成功したあとの処理
+            } catch {
+                // レスポンスのパースに失敗した場合
+                // TODO エラー処理
+                return
+            }
         }
         task.resume()
     }
 }
 
-struct Sort {
-    /// sort をクエリで指定するときの key
-    static let name = "s"
-    
-    /// 登録順
-    static let createdAt = "c"
+/// コモンズの検索APIのレスポンス
+struct NicoCommonsSearchResponse: Codable {
+    let data: NicoCommonsSearchData
 }
 
-struct Order {
-    /// order をクエリで指定する時の key
-    static let name = "o"
+struct NicoCommonsSearchData: Codable {
+    let materials: [NicoCommonsMaterial]
+}
+
+struct NicoCommonsMaterial: Codable {
+    let id: String
+    let title: String
     
-    /// 昇順
-    static let asc = "a"
-    /// 降順
-    static let desc = "d"
+    private enum CodingKeys: String, CodingKey {
+        // APIのレスポンスでは id は数字のみのID
+        // global_id は ncXXXX 形式のID
+        // nc 付きのものを使いたいので、 id を global_id とする
+        case id = "global_id"
+        case title = "title"
+    }
+}
+
+/// コモンズの検索APIのクエリを生成する
+public class QueryBuilder {
+    public init() {
+    }
+    
+    /// クエリを生成する
+    /// materialType は実際は複数選択できるが、ここでは一つしか受け付けないとする
+    public func build(
+        sort: String,
+        limit: Int = 30,
+        offset: Int = 0,
+        materialType: Int,
+        permissionScopes: [Int],
+        commercialUses: [Int]
+    ) -> [URLQueryItem] {
+        var queryItems = [
+            URLQueryItem(name: "_sort", value: sort),
+            URLQueryItem(name: "limit", value: String(limit)),
+            URLQueryItem(name: "offset", value: String(offset)),
+        ]
+        // API的には複数選択できるが、ここでは一つしか選択できない仕様とする
+        queryItems.append(URLQueryItem(
+            name: "filters[materialType][0]",
+            value: String(materialType)
+        ))
+        
+        // これらはインタフェースは複数とれるようになっているが、
+        // 複数選択できる必要が出てきてから実装する
+        queryItems.append(URLQueryItem(
+            name: "filters[permissionScope][0]",
+            value: String(permissionScopes[0])
+        ))
+        queryItems.append(URLQueryItem(
+            name: "filters[commercialUse][0]",
+            value: String(commercialUses[0])
+        ))
+        return queryItems
+    }
+}
+
+/// 作品種類
+struct MaterialType {
+    /// 音声
+    static let AUDIO = 2
+}
+
+/// 利用許可範囲
+struct PermissionScope {
+    /// インターネット全体
+    static let ALL = 1
+}
+
+/// 営利利用
+struct CommercialUse {
+    /// 営利利用可
+    static let OK = 0
+}
+
+struct Sort {
+    static let START_TIME_DESC = "-startTime"
 }
