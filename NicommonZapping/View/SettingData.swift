a32e86aff80e876f2cbee7159fce83b94bd15b6f
yoshiyuki-sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-12-30 11:13:45 +0900

    emailとpasswordを保存するようｎ

--- NicommonZapping/View/SettingData.swift
+++ NicommonZapping/View/SettingData.swift
@@ -2,12 +2,61 @@ import Foundation
 
 /// 設定を保存する
 final class SettingData: ObservableObject {
-    @Published var email: String = ""
-    @Published var password: String = ""
+    // UserDefault から値をとってくる
+    @Published var email: String = SettingRepository.shared.getEmail() {
+        // 値が変更された時は UserDefaultに に保存する
+        didSet {
+            SettingRepository.shared.setEmail(email: self.email)
+        }
+    }
+    
+    @Published var password: String = SettingRepository.shared.getPassword() {
+        didSet {
+            SettingRepository.shared.setPassword(password: self.password)
+        }
+    }
     
     /// シングルトン
     static let shared = SettingData()
     
     /// シングルトンなので外部での初期化禁止
-    private init() {}
+    private init() {
+    }
+}
+
+class SettingRepository {
+    let userDefaults: UserDefaults
+    
+    /// シングルトン
+    static let shared = SettingRepository()
+    
+    /// シングルトンなので init は private
+    private init(userDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = userDefaults
+    }
+    
+    private func getString(key: String) -> String {
+    let optional = self.userDefaults.string(forKey: key)
+        if let str = optional {
+            return str
+        } else {
+            return ""
+        }
+    }
+    
+    public func getEmail() -> String {
+        return self.getString(key: "email")
+    }
+    
+    public func setEmail(email: String) {
+        self.userDefaults.set(email, forKey: "email")
+    }
+    
+    public func getPassword() -> String {
+        return self.getString(key: "password")
+    }
+    
+    public func setPassword(password: String) {
+        self.userDefaults.set(password, forKey: "password")
+    }
 }
