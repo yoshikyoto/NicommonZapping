fb0bfa6167a973cca6e02eaf0c62f78d59cb22b5
yoshiyuki-sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-01-08 18:17:45 +0900

    starの色が変わるように

--- NicommonZapping/View/AudioList.swift
+++ NicommonZapping/View/AudioList.swift
@@ -17,9 +17,45 @@ struct PlayerView: View {
     }
 }
 
+struct Stared: View {
+    var body: some View {
+        Image(systemName: "star.fill")
+        .imageScale(.medium)
+        .foregroundColor(.yellow)
+        .frame(width: 44, height: 44)
+    }
+}
+
+struct NotStared: View {
+    var body: some View {
+        Image(systemName: "star.fill")
+        .imageScale(.medium)
+        .foregroundColor(.gray)
+        .frame(width: 44, height: 44)
+    }
+}
+
+
 struct AudioList: View {
     @EnvironmentObject var audioData: AudioData
     @State private var isShowingSettingModal = false
+    
+    func starButton(star: Star) -> AnyView {
+        switch star {
+        case .stared:
+            return AnyView(Stared().onTapGesture {
+                print("お気に入り解除")
+            })
+        case .notStared:
+            return AnyView(NotStared().onTapGesture {
+                print("お気に入り")
+            })
+        case .pending:
+            return AnyView(NotStared().onTapGesture {
+                print("pending")
+            })
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -28,14 +64,8 @@ struct AudioList: View {
                     HStack {
                         // Listの中にButtonを置くと領域全部がボタンになってしまうみたいなので
                         // ImageにonTapGestureをつけている
+                        self.starButton(star: audio.star)
                         
-                        Image(systemName: "star.fill")
-                            .imageScale(.medium)
-                            .foregroundColor(.yellow)
-                            .frame(width: 44, height: 44)
-                            .onTapGesture {
-                                print("お気に入り")
-                            }
                         // 再生はList全体に判定があって問題無いのでButtonで実装している
                         // Buttonのサイズは適当にしているが、
                         // List全体似判定が起こるのは仕様じゃないかもしれない
